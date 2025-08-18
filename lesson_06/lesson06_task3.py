c2dfac1032b710c9e8882732fd4827e1591ec91e
Juliya <galojul3872@gmail.com>
2025-08-18 17:30:43 +0300

    11 commit

---
+++ lesson_06/lesson06_task3.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(
+    ChromeDriverManager().install()))
+
+driver.get(
+    'https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+
+WebDriverWait(driver, 60).until(
+    EC.invisibility_of_element_located((By.ID, "spinner")))
+
+third_image = driver.find_element(By.ID, 'award')
+
+print(third_image.get_attribute('src'))
+
+driver.quit()
