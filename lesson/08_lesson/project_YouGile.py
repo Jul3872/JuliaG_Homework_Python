f9cfdcf8ddb89ef1525f565a1c678913d21a387b
Juliya <galojul3872@gmail.com>
2025-09-14 14:33:00 +1000

    17 commit

--- lesson/08_lesson/project_YouGile.py
+++ lesson/08_lesson/project_YouGile.py
@@ -3,34 +3,34 @@ import requests
 
 class ProjectYouGile:
     # Инициализация
-    def __init__(self, url) -> None:
+    def __init__(self, url, login, password, company) -> None:
         self.url = url
-
-    # Получить список проектов компании
-    def get_project_list(self, login, password, name):
-        payload = {
-            'login': login,
-            'password': password,
-            'name': name
-        }
-        resp = requests.post(self.url + 'auth/companies', json=payload)
-        return resp.json()
+        self.token = self.get_token(login, password, company)
 
     # Получить ключ авторизации
-    def get_token(self, login, password,
-                  companyID='0712b0a4-65ec-4443-ac24-0fd40742d8eb'):
+    def get_token(self, login, password, company):
         payload = {
             "login": login,
             "password": password,
-            "companyID": companyID
+            "companyId": company
         }
-        resp = requests.post(self.url + 'auth/keys', json=payload)
-        return resp.json()['key']
+        resp = requests.post(self.url + 'auth/keys/get', json=payload)
+
+        response_data = resp.json()
+        return response_data[0]['key']
+
+    # Получить список проектов компании
+    def get_project_list(self):
+        key = self.token
+        headers = {
+            'Authorization': f'Bearer {key}'
+        }
+        resp = requests.get(self.url + 'projects', headers=headers)
+        return resp.json()["content"]
 
     # Добавить компанию:
-    def create_project(self, title, users, login, password, companyID):
-        key = self.get_token(login=login,
-                             password=password, companyID=companyID)
+    def create_project(self, title, users):
+        key = self.token
         headers = {
             'Authorization': f'Bearer {key}',
             'Content-Type': 'application/json; charset=utf-8',
@@ -43,26 +43,22 @@ class ProjectYouGile:
         resp = requests.post(self.url + 'projects',
                              headers=headers,
                              json=project)
-        return resp.json()
+        return resp
 
-        # Получить проект по id
-    def get_project_with_id(self, project_id, login, password, companyID):
-        key = self.get_token(login=login,
-                             password=password, companyID=companyID)
+    # Получить проект по id
+    def get_project_with_id(self, project_id):
+        key = self.token
         headers = {
-            'Authorization': f'Bearer {key}',
-            'Content-Type': 'application/json; charset=utf-8',
-            'Accept': 'application/json; charset=utf-8'
+            'Authorization': f'Bearer {key}'
         }
         resp = requests.get(self.url + f'projects/{project_id}',
                             headers=headers)
-        return resp.json()
+        return resp
 
     # Изменить название проекта
     def edit_project(self, project_id, new_deleted, new_title,
-                     new_users, login, password, companyID):
-        key = self.get_token(login=login,
-                             password=password, companyID=companyID)
+                     new_users):
+        key = self.token
         headers = {
             'Authorization': f'Bearer {key}',
             'Content-Type': 'application/json; charset=utf-8',
@@ -75,4 +71,4 @@ class ProjectYouGile:
         }
         resp = requests.put(self.url + f'projects/{project_id}',
                             headers=headers, json=project)
-        return resp.json()
+        return resp
--- lesson/08_lesson/test_project_YouGile.py
+++ lesson/08_lesson/test_project_YouGile.py
@@ -12,133 +12,102 @@ TEST_USER = {USER_UUID: ADMIN_ROLE}
 TITLE_GET_TEST = "Get_Project_Test"
 TITLE_EDIT_TEST = "Edit_Project_Test"
 EDITED_TITLE = "Edited_Project_Test"
-DELETED_STATUS = "false"
-NEW_TITLE_NEGATIV = ""
-USER_UUID_NEGATIV = "95bb52b4-fa9e-44bd-80b3-84a437b66df"
-TEST_USER_NEGATIV = {USER_UUID_NEGATIV: ADMIN_ROLE}
+DELETED_STATUS = False
+NEW_TITLE_NEGATIVE = ""
+USER_UUID_NEGATIVE = "95bb52b4-fa9e-44bd-80b3-84a437b66df"
+TEST_USER_NEGATIVE = {USER_UUID_NEGATIVE: ADMIN_ROLE}
 
 # Экземпляр API
-api = ProjectYouGile('https://ru.yougile.com/api-v2/')
+api = ProjectYouGile('https://ru.yougile.com/api-v2/',
+                     LOGIN, PASSWORD, COMPANY_ID)
 
 
-def test_create_project_positiv():
-    try:
-        # Количество проектов до
-        projects_before = api.get_project_list(login=LOGIN,
-                                               password=PASSWORD, name=NAME)
-        len_before = len(projects_before)
+def test_create_project_positive():
+    # Количество проектов до
+    projects_before = api.get_project_list()
+    len_before = len(projects_before)
 
-        # Создание проекта
-        result = api.create_project(NEW_TITLE, TEST_USER,
-                                    LOGIN, PASSWORD, COMPANY_ID)
-        new_id = result['id']
+    # Создание проекта
+    result = api.create_project(NEW_TITLE, TEST_USER)
+    new_id = result.json()['id']
 
-        # Количество проектов после
-        projects_after = api.get_project_list(login=LOGIN,
-                                              password=PASSWORD, name=NAME)
-        len_after = len(projects_after)
+    # Количество проектов после
+    projects_after = api.get_project_list()
+    len_after = len(projects_after)
 
-        assert result.status_code == 201
-        assert len_after - len_before == 1
-        assert projects_after[-1]['title'] == NEW_TITLE
-        assert projects_after[-1]['id'] == new_id
-    except AssertionError as err:
-        print(f"Ошибка в тесте создания проекта: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert result.status_code == 201
+    assert len_after - len_before == 1
+    assert projects_after[-1]['title'] == NEW_TITLE
+    assert projects_after[-1]['id'] == new_id
 
 
-def test_create_project_negativ():
-    try:
-        # Количество проектов до
-        projects_before = api.get_project_list(login=LOGIN,
-                                               password=PASSWORD, name=NAME)
-        len_before = len(projects_before)
+def test_create_project_negative():
+    projects_before = api.get_project_list()
+    len_before = len(projects_before)
 
-        # Создание проекта с пустым названием
-        result = api.create_project(NEW_TITLE_NEGATIV, TEST_USER,
-                                    LOGIN, PASSWORD, COMPANY_ID)
+    # Создание проекта с пустым названием
+    result = api.create_project(NEW_TITLE_NEGATIVE, TEST_USER)
 
-        # Количество проектов после
-        projects_after = api.get_project_list(login=LOGIN,
-                                              password=PASSWORD, name=NAME)
-        len_after = len(projects_after)
+    # Количество проектов после
+    projects_after = api.get_project_list()
+    len_after = len(projects_after)
 
-        assert result.status_code == 400
-        assert len_after - len_before == 0
-    except AssertionError as err:
-        print(f"Ошибка в тесте создания проекта: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert result.status_code == 400
+    assert len_after - len_before == 0
 
 
-def test_get_project_with_id_positiv():
-    try:
-        # Создание проекта
-        result = api.create_project(TITLE_GET_TEST, TEST_USER,
-                                    LOGIN, PASSWORD, COMPANY_ID)
-        project_id = result['id']
+def test_get_project_with_id_positive():
+    result = api.create_project(TITLE_GET_TEST, TEST_USER)
+    project_id = result.json()['id']
 
-        # Обращаемся к проекту
-        new_project = api.get_project_with_id(project_id)
+    # Обращаемся к проекту
+    new_project = api.get_project_with_id(project_id)
 
-        assert new_project.status_code == 200
-        assert new_project['title'] == TITLE_GET_TEST
-        assert new_project['users'] == TEST_USER
-    except AssertionError as err:
-        print(f"Ошибка в тесте получения проекта по ID: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert new_project.status_code == 200
+    assert new_project.json()['title'] == TITLE_GET_TEST
+    assert new_project.json()['users'] == TEST_USER
+
+    # Очистка данных после теста
+    api.edit_project(project_id, True,
+                     TITLE_GET_TEST, TEST_USER)
 
 
-def test_get_project_with_id_negativ():
-    try:
-        # Создание проекта
-        api.create_project(TITLE_GET_TEST, TEST_USER,
-                           LOGIN, PASSWORD, COMPANY_ID)
+def test_get_project_with_id_negative():
+    # Обращаемся к проекту с несуществующим  id
+    new_project = api.get_project_with_id("9999")
 
-        # Обращаемся к проекту без указания id проекта
-        new_project = api.get_project_with_id()
-
-        assert new_project.status_code == 401
-    except AssertionError as err:
-        print(f"Ошибка в тесте получения проекта по ID: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert new_project.status_code == 404
 
 
-def test_edit_project_positiv():
-    try:
-        # Создание проекта
-        result = api.create_project(TITLE_EDIT_TEST, TEST_USER,
-                                    LOGIN, PASSWORD, COMPANY_ID)
-        project_id = result['id']
+def test_edit_project_positive():
+    # Создание проекта
+    result = api.create_project(TITLE_EDIT_TEST, TEST_USER)
+    project_id = result.json()['id']
 
-        # Изменение проекта
-        edited = api.edit_project(project_id, DELETED_STATUS,
-                                  EDITED_TITLE, TEST_USER)
+    # Изменение проекта
+    edited = api.edit_project(project_id, DELETED_STATUS,
+                              EDITED_TITLE, TEST_USER)
 
-        assert edited.status_code == 200
-        assert edited['title'] == EDITED_TITLE
-    except AssertionError as err:
-        print(f"Ошибка в тесте редактирования проекта: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert edited.status_code == 200
+    project_title = api.get_project_with_id(project_id).json()['title']
+    assert project_title == EDITED_TITLE
+
+    # Очистка данных после теста
+    api.edit_project(project_id, True,
+                     EDITED_TITLE, TEST_USER)
 
 
-def test_edit_project_negativ():
-    try:
-        # Создание проекта
-        result = api.create_project(TITLE_EDIT_TEST, TEST_USER,
-                                    LOGIN, PASSWORD, COMPANY_ID)
-        project_id = result['id']
+def test_edit_project_negative():
+    # Создание проекта
+    result = api.create_project(TITLE_EDIT_TEST, TEST_USER)
+    project_id = result.json()['id']
 
-        # Попытка змененить проект с неверным идентификатором роли
-        edited = api.edit_project(project_id, DELETED_STATUS,
-                                  EDITED_TITLE, TEST_USER_NEGATIV)
+    # Попытка змененить проект с неверным идентификатором роли
+    edited = api.edit_project(project_id, DELETED_STATUS,
+                              EDITED_TITLE, TEST_USER_NEGATIVE)
 
-        assert edited.status_code == 404
-    except AssertionError as err:
-        print(f"Ошибка в тесте редактирования проекта: {err}")
-    except Exception as exc:
-        print(f"Общая ошибка: {exc}")
+    assert edited.status_code == 400
+
+    # Очистка данных после теста
+    api.edit_project(project_id, True,
+                     EDITED_TITLE, TEST_USER)
